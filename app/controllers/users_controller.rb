a2fe2d77424f960e5126b6f0206a92eafe4e5485
erikn775 <erikn775@gmail.com>
2020-12-17 16:00:00 -0700

    added user.valid? to post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 
   post '/signup' do
       user = User.new(params[:user])
-      if user.save
+      if user.valid?
+        user.save
         session[:user_id] = user.id
         redirect '/account'
       else
